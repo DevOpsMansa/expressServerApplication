f4c9277edd37e3a3b7a8009cdf1c92a0948f01c1
Eric (Mansa) Marah <devopsmansa@gmail.com>
2024-02-25 14:42:52 -0600

    Eight commit: Added module and env to server

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-const express = require('express');
-
-
+const express = require("express");
+const dotenv = require(`dotenv`);
+const morgan = require('morgan');
 const app = express();
+const bodyparser = require("body-parser");
+const path = require('path');
 
-app.get(`/`,(req,res)=>{
-    res.send("Express Server Application");
-})
+dotenv.config({ path: "config.env" }); //
+const PORT = process.PORT || 8080;
 
+// log requests
+app.use(morgan('tiny'));
 
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended : true}))
 
+// set view engine
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
 
+app.get(`/`, (req, res) => {
+  res.send("Express Server Application");
+});
 
 //liststen to PORT
-app.listen(3000, ()=> { console.log(`Server is running on http://localhost:${3000}`)});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
